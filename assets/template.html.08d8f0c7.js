9cf625938591b15638cdbced63f01e6c4cf0e01c
Fgrandorder <Fgrandorder@users.noreply.github.com>
2022-08-03 09:05:41 +0000

    deploy: 7698f49f24375547f3c80bf1087b0fbfa6eb8e45

---
+++ assets/template.html.08d8f0c7.js
@@ -0,0 +1,128 @@
+import{_ as s,r as l,o as d,c as a,b as i,d as v,e as n,a as r}from"./app.45310666.js";const c={},m=i("h1",{id:"format-for-servant-material-profiles-from-materials-i-ix",tabindex:"-1"},[i("a",{class:"header-anchor",href:"#format-for-servant-material-profiles-from-materials-i-ix","aria-hidden":"true"},"#"),n(" Format for Servant Material Profiles from Materials I-IX")],-1),u=n("When finished with a profile, send the file to Lib or "),b={href:"https://github.com/r-grandorder/wiki/tree/main/src/Materials",target:"_blank",rel:"noopener noreferrer"},t=n("do a pull request"),o=r(`<div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>::: tip
+Note: [This material is translated by X](Link to translation)
+:::
+
+# ServantName
+
+## Info
+
+**Bio-Data**
+
+* True Name:
+* Class:
+* Source:
+* Region:
+* Gender:
+* Alignment:
+* Height:
+* Weight:
+
+**Meta**
+
+* Character Creator:
+* Character Illustrator:
+* Character Voice:
+* Appearance in Main Works:
+
+## Stats
+
+| Parameters | Values |
+|:--------|:--------:|
+| Strength | ? |
+| Constitution | ? |
+| Agility | ? |
+| Magical Power | ? |
+| Luck | ? |
+| Noble Phantasm | ? |
+
+## Class Skills
+
+**ClassSkill1: [A~E]**
+
+(description)
+
+**ClassSkill2: [A~E]**
+
+(description)
+
+## Personal Skills
+
+**Skill1: [A~E]**
+
+(description)
+
+(...)
+
+## Noble Phantasm(s)
+
+# NPTitle: NPSubtitle
+
+* Rank:
+* Type:
+* Range:
+* Max. Targets:
+
+(NP description)
+
+# Character
+
+## Pronouns
+
+* First Person:
+* Second Person:
+* Third Person:
+
+## Personality
+
+(...)
+
+## Attitude Towards Master
+
+(...)
+
+## Dialogue Examples
+
+&quot; (...) &quot;
+
+&quot; (...) &quot;
+
+## Historical Character and Figure
+
+(...)
+
+## Character in FGO
+
+(...)
+
+## Extra Info from In-Game Profile
+
+(Usually Bond 6, or story clear bond, etc)
+
+## Standard Weapons
+
+(...)
+
+## Related Characters
+
+**Character 1**
+
+(...)
+
+**Character 2**
+
+(...)
+
+## Comment from the Illustrator
+
+(...)
+
+## Material Images
+
+(Lib will add after publish)
+
+## Dialogue
+
+| Occassion | English | Japanese |
+|:--------|:--------:|:--------:|
+| Summoned | blahblah | \u4F55\u3068\u304B\u4F55\u3068\u304B |
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function h(f,p){const e=l("ExternalLinkIcon");return d(),a("div",null,[m,i("p",null,[u,i("a",b,[t,v(e)])]),o])}var g=s(c,[["render",h],["__file","template.html.vue"]]);export{g as default};
